fd0847bbf2a07d0786bfe3a8a989173357fc81ab
Kumassy <kumassyii@gmail.com>
2017-01-17 18:26:24 +0900

    keypress event never called😢

--- src/index.jsx
+++ src/index.jsx
@@ -15,6 +15,8 @@ class App extends React.Component {
 class Canvas extends React.Component {
   constructor (){
     super();
+
+    // this._handleKeyDown = this._handleKeyDown.bind(this);
   }
 
   componentDidMount(){
@@ -27,18 +29,45 @@ class Canvas extends React.Component {
     this.stage.addChild(circle);
     this.stage.update();
 
-    createjs.Tween.get(circle, { loop: true })
-      .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
-      .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
-      .to({ alpha: 0, y: 225 }, 100)
-      .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
-      .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
-    createjs.Ticker.setFPS(60);
-    createjs.Ticker.addEventListener("tick", this.stage);
+    // createjs.Tween.get(circle, { loop: true })
+    //   .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
+    //   .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
+    //   .to({ alpha: 0, y: 225 }, 100)
+    //   .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
+    //   .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
+    // createjs.Ticker.setFPS(60);
+    // createjs.Ticker.addEventListener("tick", this.stage);
+
+
+    const player = new createjs.Bitmap("img/kumassy.jpg");
+    this.stage.addChild(player);
+
+
+    createjs.Ticker.setFPS(30);
+    createjs.Ticker.addEventListener('tick', () => {
+        this.stage.update();
+    });
+
+
+  }
+
+  componentWillMount (){
+    window.addEventListener('keydown', this._handleKeyDown);
+    window.addEventListener('keyup', this._handleKeyDown);
+    window.addEventListener('keypress', this._handleKeyDown);
+    document.addEventListener('keydown', this._handleKeyDown);
+    document.addEventListener('keyup', this._handleKeyDown);
+    document.addEventListener('keypress', this._handleKeyDown);
+  }
+
+  _handleKeyDown (event) {
+    console.log(event);
   }
 
   render () {
-    return <canvas ref="canvas" width="1000" height="600"></canvas>;
+    return (
+      <canvas ref="canvas" width="1000" height="600"></canvas>
+    );
   }
 }
 
