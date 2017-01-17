7a8d8f671c9dde86f8cc07a9065655ca1618f560
Kumassy <kumassyii@gmail.com>
2017-01-17 15:11:07 +0900

    super basic easejs

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+// import {render, ReactDOM} from 'react-dom';
 import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render () {
-    return <p> Hello React!</p>;
+    return (<div>
+      <p> Hello React!</p>
+      <Canvas />
+    </div>);
+  }
+}
+
+class Canvas extends React.Component {
+  constructor (){
+    super();
+  }
+
+  componentDidMount(){
+   const canvas = ReactDOM.findDOMNode(this.refs.canvas);
+   this.stage = new createjs.Stage(canvas);
+   const circle = new createjs.Shape();
+   circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+   circle.x = 100;
+   circle.y = 100;
+   this.stage.addChild(circle);
+   this.stage.update();
+  }
+
+  render () {
+    return <canvas ref="canvas"></canvas>;
   }
 }
 
